caed292bb43b2583d9a4c920d86066442269e47c
zurachan <manchestervn199@gmail.com>
2023-08-05 13:45:19 +0700

    add insert admin

---
+++ insert-admin.sql
@@ -0,0 +1,4 @@
+﻿insert into chucvu values ('Admin'),(N'Lễ tân'),(N'Nhân sự'),(N'Tài chính'),(N'Dịch vụ')
+insert into quyen(tenquyen) values ('Administrator')
+insert into nhanvien(hoten, ngaysinh,diachi,gioitinh,macv,cccd,ngayvaolam,sdt) values('Admin','2023-08-01 00:00:00.0000000', 'dia chi admin', 1, 1,'cccd admin', '2023-08-01 00:00:00.0000000', 'sdt admin')
+insert into taikhoan values('admin','admin', 1, 1)
